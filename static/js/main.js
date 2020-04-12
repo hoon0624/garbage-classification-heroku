a8d00ad6f835db41725e8bf1ee48551aa5cf2cae
Donghoon Lee <donghoon.lee@mail.mcgill.ca>
2020-04-12 16:30:42 -0400

    minor fixes

--- static/js/main.js
+++ static/js/main.js
@@ -48,7 +48,7 @@ $(document).ready(function () {
                 // Get and display the result
                 $('.loader').hide();
                 $('#result').fadeIn(600);
-                $('#result').text(' Model Predict the garbage as:  ' + data);
+                $('#result').text(' Model predicts the garbage as:  ' + data);
                 console.log('Success!');
             },
         });
